bd88f16a67ecca68e68f814a04b6e56a43d4d58e
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-21 01:55:01 +0000

    ranking font

--- resources/views/ranking/have.blade.php
+++ resources/views/ranking/have.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Haveランキング</h1>
+    <h1><span style="font : normal 900 40pt 'Monotype Corsiva'">Have Ranking</span></h1>
     @include('items.items', ['items' => $items])
 @endsection
