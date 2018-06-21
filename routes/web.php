14b6204ebf6679086c07812d65f8b36f3238a2f3
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-21 01:30:24 +0000

    have ranking

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-// (10.2 add) Ranking
+// (10.2 add) WantRanking
 Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
+// (kadai add) HaveRanking
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
 
 Route::group(['middleware' => ['auth']], function () {
     // Route::resource('items', 'ItemsController', ['only' => ['create']]);
